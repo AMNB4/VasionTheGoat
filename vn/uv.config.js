9000db8b7838a2ed6a87db86571022a0992e9940
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2025-01-11 09:18:59 -0800

    Update uv.config.js

--- vn/uv.config.js
+++ vn/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://loungef2x.koyeb.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/vn/uv.handler.js',
+    bundle: '/vn/uv.bundle.js',
+    config: '/vn/uv.config.js',
+    sw: '/vn/uv.sw.js',
 };
