03ec45760e7e426ec872ed15ec5934a9588cd535
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2025-01-11 08:55:13 -0800

    Create uv.config.js

---
+++ vn/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/vn/ontop/',
+    bare:'https://loungef2x.koyeb.app/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
