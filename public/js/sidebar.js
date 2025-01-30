146f67e39df18a68373bbbb22d5a6ea384dbbd32
purp1e <118478433+AMNB4@users.noreply.github.com>
2025-01-29 19:04:38 -0800

    Update sidebar.js

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebar.innerHTML = `
     <div class="sidebar">
         <a href="/"><img src="img/home.png" alt="Home Icon"></a>
-        <a href="/&"><img src="img/games.png" alt="Game Icon"></a>
-        <a href="/@"><img src="img/apps.png" alt="Grid Icon"></a>
+        <a href="/games.html"><img src="img/games.png" alt="Game Icon"></a>
+        <a href="/apps.html"><img src="img/apps.png" alt="Grid Icon"></a>
         <a href="/!"><img src="img/settings.png" alt="Gear Icon"></a>
     </div>
     `;
