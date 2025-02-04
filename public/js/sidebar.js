b0b6b9b9f9974309272973abb39f95d94ffbd727
KindKid <95953396+kindkid27@users.noreply.github.com>
2025-02-04 07:29:06 -0600

    hotfix hotfix (I'm stupid and forgot to fix the one thing I tried to fix 😭)

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <a href="/"><img src="img/home.png" alt="Home Icon"></a>
         <a href="/g"><img src="img/games.png" alt="Game Icon"></a>
         <a href="/a"><img src="img/apps.png" alt="Grid Icon"></a>
-        <a href="/="><img src="img/settings.png" alt="Gear Icon"></a>
+        <a href="/s"><img src="img/settings.png" alt="Gear Icon"></a>
     </div>
     `;
     document.body.appendChild(sidebar);
