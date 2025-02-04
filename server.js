c3c6d2b74bc264bb7f2150b96e736b6d3748b722
KindKid <95953396+kindkid27@users.noreply.github.com>
2025-02-04 07:18:36 -0600

    Tinkering to fix the settings url

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/~', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'random.html'));
 });
 
-app.get('/!', (req, res) => {
+app.get('/=', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'settings.html'));
 });
 
